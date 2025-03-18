cf9e423243dfebbdd1ed5e6177ed135c03617bb9
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-03-18 17:29:16 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
 function myButtonClicked() {
-  alert("Hello, World!")
+  const length = document.getElementById("length").value
+  const width = document.getElementById("width").value
+  const depth = document.getElementById("depth").value
+  document.getElementById("answer").innerHTML = 
+    "The volume is: " + (length*width*depth)
 }
